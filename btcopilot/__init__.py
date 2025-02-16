6dedc703f6643a9bb62df477c154d3f574263ecc
Patrick Stinson <patrickkidd@gmail.com>
2025-02-16 08:14:22 -0900

    back to mistral

--- btcopilot/__init__.py
+++ btcopilot/__init__.py
@@ -1,7 +1,12 @@
 EMBEDDINGS_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-# LLM_MODEL = "mistral"
+
+# Best so far
+LLM_MODEL = "mistral"
+
 # LLM_MODEL = "mistral:7b-text-q8_0"
-LLM_MODEL = "tinyllama"
+
+# LLM_MODEL = "tinyllama" # Had dirty answers
+
 # LLM_MODEL = "deepseek-r1:14b"
 # LLM_MODEL = "deepseek-r1:1.5b"
 
