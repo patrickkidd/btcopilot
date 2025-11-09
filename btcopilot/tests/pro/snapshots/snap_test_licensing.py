e43be755e72f7124b0c3663ad306301571c4bd52
Patrick Stinson <patrickkidd@gmail.com>
2025-11-08 20:37:05 -0900

    fix test

--- btcopilot/tests/pro/snapshots/snap_test_licensing.py
+++ btcopilot/tests/pro/snapshots/snap_test_licensing.py
@@ -176,7 +176,7 @@ snapshots['test_users_email_code 1'] = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHT
 <div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:25px; padding-bottom:25px; padding-right: 0px; padding-left: 25px;">
 <!--<![endif]-->
 <div align="center" class="img-container center autowidth" style="padding-right: 0px;padding-left: 0px;">
-<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]--><img align="center" alt="Image" border="0" class="center autowidth" src="http://127.0.0.1/v1/static/email-assets/vedana-logo-black2x.png" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 300px; display: block;" title="Image" width="300"/>
+<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]--><img align="center" alt="Image" border="0" class="center autowidth" src="https://alaskafamilysystems.com/wp-content/static/FullColor_1280x1024_72dpi.jpg" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 300px; display: block;" title="Image" width="300"/>
 <!--[if mso]></td></tr></table><![endif]-->
 </div>
 <!--[if (!mso)&(!IE)]><!-->
@@ -220,7 +220,7 @@ snapshots['test_users_email_code 1'] = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHT
 <!--<![endif]-->
 <div align="center" class="img-container center fixedwidth" style="padding-right: 0px;padding-left: 0px;">
 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]-->
-<div style="font-size:1px;line-height:45px">\xa0</div><img align="center" alt="Image" border="0" class="center fixedwidth" src="http://127.0.0.1/v1/static/email-assets/illo_welcome_1.png" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 540px; display: block;" title="Image" width="540"/>
+<div style="font-size:1px;line-height:45px">\xa0</div><img align="center" alt="Image" border="0" class="center fixedwidth" src="https://alaskafamilysystems.com/wp-content/static/illo_welcome_1.png" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 540px; display: block;" title="Image" width="540"/>
 <!--[if mso]></td></tr></table><![endif]-->
 </div>
 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 15px; padding-top: 20px; padding-bottom: 0px; font-family: Tahoma, Verdana, sans-serif"><![endif]-->
