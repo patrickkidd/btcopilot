6c15a2b1e27570ba08e0e157c59ce6de97b2b446
Patrick Stinson <patrickkidd@gmail.com>
2025-11-18 14:53:22 -0900

    mm/dd/yyyy

--- btcopilot/training/__init__.py
+++ btcopilot/training/__init__.py
@@ -1,8 +1,20 @@
 from . import routes
 
 
+def format_date_us(value):
+    """Format date as mm/dd/yyyy instead of yyyy-mm-dd"""
+    if value is None:
+        return ''
+    if hasattr(value, 'strftime'):
+        # It's a datetime/date object
+        return value.strftime('%m/%d/%Y')
+    # It's a string in ISO format
+    return value
+
+
 def init_app(app):
     app.register_blueprint(routes.bp)
+    app.jinja_env.filters['format_date_us'] = format_date_us
 
 
 def init_celery(celery):
